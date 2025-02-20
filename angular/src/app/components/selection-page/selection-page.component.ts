89efc262fc3299bf7c272d83b1e1b45ca7b0c1a5
bakeKeba <bakir.sabljica@gmail.com>
2025-02-20 23:29:14 +0100

    finished selection page, cart page remaining

--- angular/src/app/components/selection-page/selection-page.component.ts
+++ angular/src/app/components/selection-page/selection-page.component.ts
@@ -1,33 +1,62 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';  // Import ActivatedRoute
+import { ActivatedRoute, Router } from '@angular/router';  // Import ActivatedRoute
 import { ProductService } from '../../services/product.service';
-import { NgIf } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { HeaderComponent } from '../header/header.component';
+import { CartService } from '../../services/cart.service';
 
 @Component({
     selector: 'app-selection-page',
-    imports: [NgIf, HeaderComponent],
+    imports: [NgIf, HeaderComponent, NgFor],
     standalone: true,
     templateUrl: './selection-page.component.html',
     styleUrls: ['./selection-page.component.css']
 })
 export class SelectionPageComponent implements OnInit {
     shoe: any;
+    sizes: number[] = [38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49]; // Shoe sizes
+    selectedSize: number | null = null;
+    selectedImage: string = "";
 
     constructor(
-        private router: ActivatedRoute,
-        private productService: ProductService
+        private router: Router,
+        private activatedRoute: ActivatedRoute,
+        private productService: ProductService,
+        private cartService: CartService
     ) { }
 
     ngOnInit() {
-        const shoeId = this.router.snapshot.paramMap.get('id');
+        const shoeId = this.activatedRoute.snapshot.paramMap.get('id');
 
         if (shoeId) {
             this.productService.getProductById(Number(shoeId)).subscribe((product) => {
                 this.shoe = product;
+                this.selectedImage = this.shoe.images[0];
+                console.log(this.shoe.images);
             });
         } else {
             console.log('Product ID is missing');
         }
+    }
+
+    addToCart(productId: number) {
+        if (!this.selectedSize) return;
+
+        this.cartService.addToCart(productId).subscribe({
+            next: () => {
+                this.router.navigate(['/']);
+            },
+            error: (err) => {
+                console.error('Error adding to cart:', err);
+            }
+        });
+    }
+
+    changeImage(image: string) {
+        this.selectedImage = image;
+    }
+
+    selectSize(size: number) {
+        this.selectedSize = size;
     }
 }
